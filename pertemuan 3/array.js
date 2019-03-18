2e1c8972979caecc5dc4661e794353acf006975c
chrisman14 <chrismanhutagalung@gmail.com>
2019-03-18 20:50:56 +0700

    materi ke 3

---
+++ pertemuan 3/array.js
@@ -0,0 +1,14 @@
+let deret=["a","b","c","d"]
+console.log(deret)
+// for (var i=0;i<deret.length;i++){
+//     console.log(deret[i])
+// }
+// deret.forEach(deretx => {
+//     console.log(deretx)
+// });
+
+var i=0
+while (i<deret.length){
+    console.log(deret[i])
+    i++
+}
---
+++ pertemuan 3/arrfu.js
@@ -0,0 +1,38 @@
+var hewan = ['hewan', 'buaya', 'kadal', 'ular','singa','entok']
+// function tampil(deret) {
+//     for (var i = 0; i < deret.length; i++) {
+//         console.log(deret[i])
+//     }
+// }
+// tampil(hewan)
+
+// function tambahHewan(){
+//     for (var i = 0; i < deret.length; i++) {
+//        hewan.pushjjj
+//     }
+// }
+
+// function hapusAkhir(){
+//     // var dump=hewan.length
+//     // hewan[dump]=''
+
+//     console.log(hapusajah())
+// }
+
+// hapusAkhir()
+// function hapusajah(){
+//     var dump=[]
+//     for (var i = 0; i < hewan.length-1; i++) {
+//         dump[i]=hewan[i]
+//     }
+//     return dump
+// }
+function bilangan_genap(){
+    for (var i = 0; i < hewan.length; i++) {
+        if(i%2==0){
+            console.log(hewan[i])
+        }
+       
+    }
+}
+bilangan_genap()
